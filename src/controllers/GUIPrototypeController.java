cc124dba9025e0072d1f93a2cd4fdf664040fa59
cdjohnnatha <cdjohnnatha@gmail.com>
2016-11-13 22:07:35 -0300

    created controller and liked buttons

---
+++ src/controllers/GUIPrototypeController.java
@@ -0,0 +1,46 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+
+public class GUIPrototypeController {
+	@FXML
+	private Slider mascaraButton;
+	@FXML
+	private Button mediaButton;
+	@FXML
+	private Button medianaButton;
+	@FXML 
+	private Button convolucaoButton;
+	@FXML
+	private Button expansaoButton;
+	@FXML
+	private Button equalizacaoButton;
+	@FXML
+	private Slider limiarizacaoSlider;
+	@FXML
+	private Button contrasteButton;
+	@FXML
+	private Button sobelButton;
+	@FXML
+	private Slider bandasSlider;
+	@FXML
+	private Button rbgyiqButton;
+	@FXML
+	private Button yiqrgbButton;
+	@FXML
+	private Button negativoButton;
+	@FXML
+	private Slider cBrilhoSlider;
+	@FXML
+	private Button multiplicativoButton;
+	@FXML
+	private Button aditivoButton;
+	
+	
+	
+	
+	
+	
+}
