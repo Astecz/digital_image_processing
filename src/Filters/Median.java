9623f90ad8b861b24fab3dcd788c746a67bb712d
Jorismar <jorismar.barbosa@gmail.com>
2016-11-14 13:12:40 -0300

    Median and Sobel Gradient filters added.

--- src/Filters/Median.java
+++ src/Filters/Median.java
@@ -5,7 +5,9 @@
  */
 package Filters;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
+import java.util.Arrays;
 
 /**
  *
@@ -14,6 +16,35 @@ import java.awt.image.BufferedImage;
 public class Median extends Filter {
     @Override
     protected int transform(BufferedImage img, int px, int py, int width) {
-        return 0;
+        int matsize = width * width;
+        
+        int[] r = new int[matsize];
+        int[] g = new int[matsize];
+        int[] b = new int[matsize];
+        
+        Arrays.fill(r, 0);
+        Arrays.fill(g, 0);
+        Arrays.fill(b, 0);
+        
+        for (int x = px - Math.round((float) width/2), count = 0; x < px + (width/2); x++)
+            for (int y = py - Math.round((float) width/2); y < py + (width/2); y++) {
+                if (x >= 0 && x < img.getWidth() && y >= 0 && y < img.getHeight()) {
+                    Color color = new Color(img.getRGB(x, y), true);
+                    
+                    r[count] = color.getRed();
+                    g[count] = color.getGreen();
+                    b[count] = color.getBlue();
+                    
+                    count++;
+                }
+                // Moved to inside of if, increment of count while pixel position is invalid result in black pixels.
+                // count++; 
+            }
+        
+        Arrays.sort(r);
+        Arrays.sort(g);
+        Arrays.sort(b);
+        
+        return new Color(r[r.length/2], g[g.length/2], b[b.length/2]).getRGB();
     }
 }
